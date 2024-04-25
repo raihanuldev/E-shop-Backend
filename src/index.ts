98df642bba8e9749ac23e49ba6da3709c43c7119
raihanuldev <rihanthedev@gmail.com>
2024-04-25 17:05:32 +0600

    database connected

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,42 @@
 import express, { Express, Request, Response } from "express";
-const dotenv = require('dotenv');
-
-dotenv.config();
+import cors from "cors";
+import { MongoClient, ServerApiVersion } from "mongodb";
+require("dotenv").config();
 
 const app: Express = express();
-const port = process.env.PORT;
+const port = 5000;
+// middilware
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req:Request, res:Response) => {
-  res.send('Express + TypeScript Server');
+console.log(process.env.DB_USER);
+console.log(process.env.DB_PASS);
+
+const uri = process.env.DB_URL as string;
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
 });
 
-app.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`);
+async function run() {
+  try {
+    await client.connect();
+    
+
+    
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    app.listen(port, () => {
+      console.log(`[server]: Server is running at http://localhost:${port}`);
+    });
+  } finally {
+  }
+}
+run().catch(console.dir);
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hey Welcome your server is running so good");
 });
